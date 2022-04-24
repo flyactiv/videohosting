6d53fa38124fb4a79a61deed3868eea40f378a26
Flyactiv <flyactiv46@gmail.com>
2022-04-24 11:11:55 +0300

    Add a video from aside block

--- views/blocks/aside.php
+++ views/blocks/aside.php
@@ -5,7 +5,16 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque cum doloremque dolores earum?</p>
     </div>
     <div class="p-3 mb-3">
-        <img class="img-thumbnail" src="https://activation-keys.ru/wp-content/uploads/2019/03/fcb710093aa005a868c24a7048d2a18d.jpg"  alt=""><br>
-        <img class="mt-3 img-thumbnail" src="https://blog.calltouch.ru/wp-content/uploads/2020/12/image2-13.png"  alt="">
+        <video class="mt-1" width="270" height="190" controls="controls" poster="https://activation-keys.ru/wp-content/uploads/2019/03/fcb710093aa005a868c24a7048d2a18d.jpg">
+
+            <source src="../views/files/12345.mp4" type="video/mp4" codecs="avc1.42E01E, mp4a.40.2">
+
+        </video>
+        <br>
+        <video class="mt-3" width="270" height="190" controls="controls" poster="https://blog.calltouch.ru/wp-content/uploads/2020/12/image2-13.png">
+
+            <source src="../views/files/gbfhjkl.mp4" type="video/mp4" codecs="avc1.42E01E, mp4a.40.2">
+
+        </video>
     </div>
 </aside>
